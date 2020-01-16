507e96d5443c9d5b0e31da78f74af64634862799
Joaquim Rabelo <joaquimrabello@gmail.com>
2020-01-16 15:30:50 -0300

    mobile: app base

--- backend/src/controllers/SearchController.js
+++ backend/src/controllers/SearchController.js
@@ -4,7 +4,6 @@ const parseStringAsArray = require('../utils/parseStringAsArray');
 module.exports = {
     async index(request, response) {
         const { latitude, longitude, techs } = request.query;
-
         const techsArray = parseStringAsArray(techs);
         
         const devs = await Dev.find({
